26f6c76989cc4977a0fa54eebf11e0add2552d34
RodrigoZafra <rodrifdt17@gmail.com>
2022-05-19 20:34:31 +0200

    Initial commit

---
+++ src/LaminateMap.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class LaminateMap extends JPanel {
+    private ButtonGroup group;
+
+    public LaminateMap(String titulo, String[] options) {
+        setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), titulo));
+
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        group = new ButtonGroup();
+
+        for (int i = 0; i < options.length; i++) {
+            JRadioButton radioButton = new JRadioButton(options[i]);
+            radioButton.setActionCommand(options[i]);
+            add(radioButton);
+            group.add(radioButton);
+            radioButton.setSelected(i == 0);
+        }
+    }
+
+    public String giveSelectionMap() {
+        return group.getSelection().getActionCommand();
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Marco marco1 = new Marco();
+        marco1.setVisible(true);
+        marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
